e9f0d34752c959865f2d120cb03b9df9e8e85875
mohithgupta <>
2020-11-28 17:42:45 +0530

    layout version1 done

---
+++ index.js
@@ -0,0 +1,8 @@
+// const section1 = document.getElementById("section1-id");
+// window.addEventListener("scroll",function(e){
+// 	console.log("scrolled");
+// 	console.log(window.pageYOffset); // getting equal to 0
+// 	section1.style.opacity="0"
+// 	section1.style.backgroundSize = 120 - +window.pageYOffset/12 +"%";
+// 	section1.style.opacity = 1- +window.pageYOffset/700 + "" ;
+// },true);
